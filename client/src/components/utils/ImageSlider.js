4a5850c05a1be54e45701271231f23ba9a780a3f
Bharath Raj R <msrbharathraj@gmail.com>
2020-04-23 00:43:03 +0530

    uploads path corrected

--- client/src/components/utils/ImageSlider.js
+++ client/src/components/utils/ImageSlider.js
@@ -9,7 +9,7 @@ function ImageSlider(props) {
                 {props.images.map((image, index) => (
                     <div key={index}>
                         <img style={{ width: '100%', maxHeight: '150px' }}
-                            src={`http://vilambarams.in/${image}`} alt="productImage" />
+                            src={`http://vilambarams.in/uploads/${image}`} alt="productImage" />
                     </div>
                 ))}
             </Carousel>
